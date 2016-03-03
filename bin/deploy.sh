f7c1f8496a8350778e8c12cc4718ecc29ab5b2b6
Giorgos Logiotatidis <seadog@sealabs.net>
2016-03-03 16:45:41 +0200

    Don't ping NR.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,8 +10,3 @@ docker push ${DOCKER_REPOSITORY}:last_successful_build
 curl -sSL http://deis.io/deis-cli/install.sh | sh
 ./deis login $DEIS_CONTROLLER  --username $DEIS_USERNAME --password $DEIS_PASSWORD
 ./deis pull ${DOCKER_IMAGE_TAG} -a $1
-curl -H "x-api-key:$NEWRELIC_API_KEY" \
-     -d "deployment[app_name]=$1" \
-     -d "deployment[revision]=$CIRCLE_SHA1" \
-     -d "deployment[user]=Travis" \
-     https://api.newrelic.com/deployments.xml
